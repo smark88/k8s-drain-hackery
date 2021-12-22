641d8832cf014ef871dbb7e77bc9ca7002ed3198
smark88 <msarcevicz@influxdata.com>
2021-12-22 11:54:22 -0500

    added colors
    added colors 2
    
    added grep filter on describe to only show namespace fields and added retry for searching namespaces
    
    comments
    
    added flags for running
    
    added retry
    
    added retry and flags for retry
    
    working sed
    
    working with proper sed
    
    added status of nodes remaining
    
    added status
    
    added exit
    
    added exit
    
    re org
    
    removed test.sh
    
    added gitignore
    
    added gitignore for lastrun
    
    fixed spacing
    
    added to-do
    
    fixed if statement
    
    added cleanup
    
    comments

--- rotate_node_pools.sh
+++ rotate_node_pools.sh
@@ -1,29 +1,11 @@
 #!/bin/bash
 set -o errexit -o pipefail -o nounset
 
-# global vars
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-PROJECTDIR=`pwd`
-UNSCHEDULEABLE="false"
-
-# drain vars
-NODEPOOL_LABEL="awslabeler.influxdata.com/type=compute"
-NODE_VERSION_TO_DRAIN="1.18.20"
-REGION="us-east-1"
-
-# defaults
-INITIAL_NODE_COUNT=0
-nodeErrorCode=0
-drainErrorCode=0
-
-# list of namespaces that should not be on a drained node
-NODE_ACTIVE_NAMESPACES=(argo-events argo-observer argocd argowf cert-manager cloudability cluster-autoscaler computation external-dns functionsd glance ingress iox istio-operator istio-system kafka-operator keda kube-node-lease kube-public kvmigrate newrelic-minion notebooksd ops pinneditemsd pipescope reboot-coordinator sampleappd sealed-secrets telegraf-operator twodotoh vault vault-secrets-manager velero)
-
-
 ## Process
 # 1. set nodepool via label and region
 # 2. login to aws cli
-# 3. run script
+# 3. run script `▶ sh rotate_node_pools.sh -n 'highmem' -v '1.18.20' -r 'us-east-1'`
+# 4. re try run script `▶ sh rotate_node_pools.sh -n 'highmem' -v '1.18.20' -r 'us-east-1' -x 'true'`
 
 ## Script process/ordering
 # 1. drains single node from nodepool
@@ -33,10 +15,31 @@ NODE_ACTIVE_NAMESPACES=(argo-events argo-observer argocd argowf cert-manager clo
 #       b. check it has been replaced by new node before continuing
 # 4. repeat step 1 for next node
 
+# global vars
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+PROJECTDIR=`pwd`
+UNSCHEDULEABLE="false"
+red=$'\e[1;31m'
+green=$'\e[1;32m'
+blue=$'\e[1;34m'
+end=$'\e[0m'
+check_drained_counter=0
+terminate_node_counter=0
+NODES=""
+
+
+# defaults
+INITIAL_NODE_COUNT=0
+nodeErrorCode=0
+drainErrorCode=0
+RETRY=false
+
+# list of namespaces that should not be on a drained node
+# To-do invert this list to what is espected
+NODE_ACTIVE_NAMESPACES=(argo-events argo-observer argocd argowf cert-manager cloudability cluster-autoscaler computation external-dns functionsd glance ingress iox istio-operator istio-system kafka-operator keda kube-node-lease kube-public kvmigrate newrelic-minion notebooksd ops pinneditemsd pipescope sampleappd sealed-secrets telegraf-operator twodotoh vault vault-secrets-manager velero)
+
 drain_nodes() {
-    printf "Draining nodes for nodes with label $NODEPOOL_LABEL\n"
-    # get nodes in pool by label count intial ready nodes
-    for i in $(kubectl get node -l $NODEPOOL_LABEL --no-headers | grep $NODE_VERSION_TO_DRAIN | awk '{print $1}'); do 
+    for i in $NODES; do 
     kubectl drain $i --delete-emptydir-data  --ignore-daemonsets --as=admin
     sleep 5
     # double drain as its cheap insurence and should happen quick the second time
@@ -44,7 +47,7 @@ drain_nodes() {
     # check second drain exit status if non 0 exit
     drainErrorCode=$?
     if [ $drainErrorCode -ne 0 ]; then
-        printf "kubectl drain node command exited with non 0"
+        printf "%s\n" "${red}Kubectl drain node command exited with non 0${end}"
         exit 126
     fi
     # check if node is unschedulable and check if any namespace from exclusion list are active within `k describe node`
@@ -58,18 +61,27 @@ check_drained () {
     UNSCHEDULEABLE=$(kubectl get node $1 -o=jsonpath='{.spec.unschedulable}')
     # check if unschedulable
     if [ "$UNSCHEDULEABLE" == "true" ] ; then
-        printf "node is unschedulable"
-        printf "check for active namespaces on node $1\n"
-        NODEDESCRIPTION=$(kubectl describe node $1)
+        printf "%s\n" "${blue}Node is unschedulable${end}"
+        printf "%s\n" "${blue}Check for active namespaces on node $1 ${end}"
+        ACTIVE_POD_COUNT=$(kubectl describe node $1 | grep "Non-terminated Pods" | grep -Eo '[0-9]{1,3}')
+        # filters out junk messages and only prints Non-terminated Pods from the grep
+        NODEDESCRIPTION=$(kubectl describe node $1 | grep -A$ACTIVE_POD_COUNT "Namespace")
         # check for namespace from exclusion list
         for a in ${NODE_ACTIVE_NAMESPACES[@]}; do
         if [[ $NODEDESCRIPTION == *"$a"* ]]; then
-            printf "Namespace $a present on node check -- FAIL\n"
+            printf "%s\n" "${red}Status -- Namespace $a present on node check -- FAIL ${end}"
+            check_drained_counter=$((check_drained_counter+1))
+            # retry if nodes arent fully drain from race condition
+            check_drained
+            sleep 10
+        elif [[ $NODEDESCRIPTION == *"$a"* && "$check_drained_counter" -gt 6 ]]; then
+            printf "%s\n" "${red}Status -- Namespace $a present on node check -- FAIL ${end}"
+            printf "%s\n" "${red}Status -- Counter: $check_drained_counter times reached; Exiting loop!${end}"
             exit 1
         fi
         done
     else     
-        printf "node should be unschedulable if it was drained" 
+        printf "%s\n" "${red}Node should be unschedulable if it was drained${end}" 
         exit 1
     fi
 }
@@ -81,7 +93,7 @@ terminate_node () {
     if [ "$UNSCHEDULEABLE" == "true" ] ; then 
         # get aws ec2 instance name for termination
         NODE=$(kubectl  get node $K8_NODE_NAME -o jsonpath='{.spec.providerID}' | awk -F/ '{print $5}')
-        printf "terminating node: $NODE via aws cli"
+        printf "%s\n" "${blue}Terminating node: $NODE via aws cli${end}"
         # check function gets drained node
         check_node () {
             # no exit on this command but save error code to pass along to other function
@@ -99,32 +111,61 @@ terminate_node () {
         check_termination () {
             check_node
             if [ $nodeErrorCode -ne 0 ]; then
-                printf "k8 Node Name: $K8_NODE_NAME EC2 Node Name: $NODE has been terminated\n"
+                printf "%s\n" "${blue}k8 Node Name: $K8_NODE_NAME EC2 Node Name: $NODE has been terminated ${end}"
+                # remove node from lastrun.txt as it has been terminated
+                sed -i "" "/$K8_NODE_NAME/d" $SCRIPTDIR/lastrun.txt
+                NODES_REMAINING=$(cat $SCRIPTDIR/lastrun.txt | wc -l)
+                printf "%s\n" "${green}Status -- Nodes Remaining: $NODES_REMAINING ${end}"
+            elif [[ "$terminate_node_counter" -gt 10 ]]; then
+                printf "%s\n" "${red}Counter: $terminate_node_counter times reached; Exiting loop!${end}"
+                exit 1
             else 
-                printf "pause 15s while k8 Node Name: $K8_NODE_NAME EC2 Node Name: $NODE is being terminated\n"
+                printf "%s\n" "${blue}Pause 15s while k8 Node Name: $K8_NODE_NAME EC2 Node Name: $NODE is being terminated ${end}"
                 sleep 15
+                terminate_node_counter=$((terminate_node_counter+1))
                 check_termination
             fi
         }
         check_termination
-        # recursive shit function to check new node is up before proceeding to avoid racing condition
-        check_node_count () {
-            # grep -v to exclude "NotReady"
-            CURRENT_NODE_COUNT=$(kubectl get node -l $NODEPOOL_LABEL --no-headers | grep -v NotReady | grep Ready | wc -l)  
-            if [ $CURRENT_NODE_COUNT -eq $INITIAL_NODE_COUNT ]; then
-                printf "Continuing new node is up\n"
-            else 
-                printf "pause 15s wait for new node to be deployed\n"
-                sleep 15
-                check_node_count
-            fi
-         
-        }
-        check_node_count
     else
-        printf "node should be unschedulable for ec2 termination" 
+        printf "%s\n" "${red}Node should be unschedulable for ec2 termination${end}" 
         exit 1
     fi
 }
 
-drain_nodes
+# clean up lastrun.txt if empty
+cleanup () {
+    [ -s lastrun.txt ] || rm -f $SCRIPTDIR/lastrun.txt
+}
+
+while getopts n:v:r:x: flag
+do
+    case "${flag}" in
+        n) NODEPOOL_NAME=${OPTARG};;
+        v) NODE_VERSION_TO_DRAIN=${OPTARG};;
+        r) REGION=${OPTARG};;
+        x) RETRY=${OPTARG};;
+    esac
+done
+
+
+# drain vars
+NODEPOOL_LABEL="awslabeler.influxdata.com/type=$NODEPOOL_NAME"
+
+if [ $RETRY == false ]; then
+    printf "%s\n" "${green}Draining nodes for nodes with label $NODEPOOL_LABEL ${end}"
+    # get nodes in pool by label count intial ready nodes
+    NODES=$(kubectl get node -l $NODEPOOL_LABEL --no-headers | grep $NODE_VERSION_TO_DRAIN | awk '{print $1}')
+    echo $NODES > $SCRIPTDIR/lastrun.txt
+    drain_nodes
+    cleanup
+elif [[ -f "$SCRIPTDIR/lastrun.txt" && $RETRY == true ]]; then
+    printf "%s\n" "${green}Retrying to drain nodes for nodes with label $NODEPOOL_LABEL from lastrun.txt ${end}"
+    # get nodes in pool by label count intial ready nodes
+    NODES=$(cat $SCRIPTDIR/lastrun.txt)
+    drain_nodes
+    cleanup
+else
+    printf "%s\n" "${red}Status -- Check for retry file or args something went terrible wrong${end}"
+    cleanup
+fi
